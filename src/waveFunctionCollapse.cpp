cf667480993a7ec93f231fd6e461898c310be3cf
Zhi Liang <zhikang8@hotmail.co.uk>
2022-11-04 19:25:07 +0000

    Wrote simple loop to collapse all tiles

--- src/waveFunctionCollapse.cpp
+++ src/waveFunctionCollapse.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
+#include <stdlib.h>
 #include <stdio.h>
 #include <string>
+#include <time.h>
 #include <utility>
 #include <vector>
 #include <map>
@@ -36,7 +39,19 @@ std::map<std::string, std::vector<std::vector<char>>> edgeMap = {
 };
 
 int main() {
+  srand(time(NULL));
   Board* board = new Board();
-  board->printBoard();
+
+  while(!board->isFinalised()) {
+    std::pair<int, int> coords = board->findTileWithLeastEntropy();
+    std::cout << coords.first << " " << coords.second << std::endl;
+    if(coords.first == -1) {
+      board->collapseRandomTile();
+    } else {
+      board->collapseTile(coords.first, coords.second);
+    }
+    board->printBoard();
+    std::cout << std::endl;
+  }
   return 0;
 }
