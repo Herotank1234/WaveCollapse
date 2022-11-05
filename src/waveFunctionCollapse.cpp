b87778d74576151a4ffe81057e1bc2b6899f07d7
Zhi Liang <zhikang8@hotmail.co.uk>
2022-11-05 15:19:40 +0000

    Moved edgeRules

--- src/waveFunctionCollapse.cpp
+++ src/waveFunctionCollapse.cpp
@@ -9,45 +9,6 @@
 #include "board.h"
 #include "tile.h"
 
-std::map<std::string, std::vector<std::string>> edgeRules = {
-  {"Black", {"bbb", "bbb", "bbb", "bbb"}},
-  {"Connector", {"bbb", "bgg", "glg", "ggb"}}, 
-  {"Connector90", {"ggb", "bbb", "bgg", "glg"}}, 
-  {"Connector180", {"glg", "ggb", "bbb", "bgg"}},
-  {"Connector270", {"bgg", "glg", "ggb", "bbb"}},
-  {"ConnectorEdge", {"ggb", "bgg", "ggg", "ggg"}},
-  {"ConnectorEdge90", {"ggg", "ggb", "bgg", "ggg"}},
-  {"ConnectorEdge180", {"ggg", "ggg", "ggb", "bgg"}},
-  {"ConnectorEdge270", {"bgg", "ggg", "ggg", "ggb"}},
-  {"DiagPipeOne", {"ggg", "glg", "glg", "ggg"}},
-  {"DiagPipeOne90", {"ggg", "ggg", "glg", "glg"}},
-  {"DiagPipeOne180", {"glg", "ggg", "ggg", "glg"}},
-  {"DiagPipeOne270", {"glg", "glg", "ggg", "ggg"}},
-  {"DiagPipeTwoL", {"glg", "glg", "glg", "glg"}},
-  {"DiagPipeTwoR", {"glg", "glg", "glg", "glg"}},
-  {"Dot", {"ggg", "ggg", "glg", "ggg"}},
-  {"Dot90", {"ggg", "ggg", "ggg", "glg"}},
-  {"Dot180", {"glg", "ggg", "ggg", "ggg"}},
-  {"Dot270", {"ggg", "glg", "ggg", "ggg"}},
-  {"DotTwoWayH", {"ggg", "glg", "ggg", "glg"}},
-  {"DotTwoWayV", {"glg", "ggg", "glg", "ggg"}},
-  {"Green", {"ggg", "ggg", "ggg", "ggg"}},
-  {"PipeCrossH", {"glg", "gyg", "glg", "gyg"}},
-  {"PipeCrossV", {"gyg", "glg", "gyg", "glg"}},
-  {"PipeGreenH", {"ggg", "glg", "ggg", "glg"}},
-  {"PipeGreenV", {"glg", "ggg", "glg", "ggg"}},
-  {"PipeGreyH", {"ggg", "gyg", "ggg", "gyg"}},
-  {"PipeGreyV", {"gyg", "ggg", "gyg", "ggg"}},
-  {"PipeSwitch", {"ggg", "gyg", "ggg", "glg"}},
-  {"PipeSwitch90", {"glg", "ggg", "gyg", "ggg"}},
-  {"PipeSwitch180", {"ggg", "glg", "ggg", "gyg"}},
-  {"PipeSwitch270", {"gyg", "ggg", "glg", "ggg"}},
-  {"PipeT", {"glg", "glg", "glg", "ggg"}},
-  {"PipeT90", {"ggg", "glg", "glg", "glg"}},
-  {"PipeT180", {"glg", "ggg", "glg", "glg"}},
-  {"PipeT270", {"glg", "glg", "ggg", "glg"}}
-};
-
 int main() {
   srand(time(NULL));
   Board* board = new Board();
