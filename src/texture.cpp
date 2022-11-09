c1acf7fffb1a91c9a7002ab2d785d0e3f4bd647a
Zhi Liang <zhikang8@hotmail.co.uk>
2022-11-09 12:40:20 +0000

    Changed to 2d texture array

--- src/texture.cpp
+++ src/texture.cpp
@@ -1,28 +1,41 @@
 #include "texture.h"
+#include <iostream>
 
-Texture::Texture() {
+Texture::Texture(std::map<std::string, ImageData*> imageMap) {
   glGenTextures(1, &_ID);
   glActiveTexture(GL_TEXTURE0);
-  glBindTexture(GL_TEXTURE_2D, _ID);
+  glBindTexture(GL_TEXTURE_2D_ARRAY, _ID);
 
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+  int width = imageMap.begin()->second->width,
+    height = imageMap.begin()->second->height;
+  glTexImage3D(GL_TEXTURE_2D_ARRAY, 0, GL_RGB, width, height, imageMap.size(),
+    0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+  
+  int index = 0;
+  for(auto it = imageMap.begin(); it != imageMap.end(); it++) {
+    glTexSubImage3D(GL_TEXTURE_2D_ARRAY, 0, 0, 0, index, width, height, 1, GL_RGB,
+      GL_UNSIGNED_BYTE, it->second->bytes);
+    _layerMap.insert({it->first, index++});
+  }
+  glBindTexture(GL_TEXTURE_2D_ARRAY, 0);
 }
 
-void Texture::changeTexture(ImageData* imgData) {
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, imgData->width, imgData->height,
-    0, GL_RGB, GL_UNSIGNED_BYTE, imgData->bytes);
+std::map<std::string, int> Texture::getLayerMap() {
+  return _layerMap;
 }
 
 void Texture::bindTexture() {
-  glBindTexture(GL_TEXTURE_2D, _ID);
+  glBindTexture(GL_TEXTURE_2D_ARRAY, _ID);
 }
 
 void Texture::unbindTexture() {
-  glBindTexture(GL_TEXTURE_2D, 0);
+  glBindTexture(GL_TEXTURE_2D_ARRAY, 0);
 }
 
 void Texture::deleteTexture() {
--- src/texture.h
+++ src/texture.h
@@ -2,16 +2,19 @@
 #define TEXTURE_H
 #include "../lib/glad/include/glad/glad.h"
 #include "structs.h"
+#include <string>
+#include <map>
 
 class Texture {
 public:
-  Texture();
-  void changeTexture(ImageData* imgData);
+  Texture(std::map<std::string, ImageData*> imageData);
+  std::map<std::string, int> getLayerMap();
   void bindTexture();
   void unbindTexture();
   void deleteTexture();
 private:
   GLuint _ID;
+  std::map<std::string, int> _layerMap;
 };
 
 #endif
